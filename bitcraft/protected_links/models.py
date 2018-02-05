c5b6dbbf8d253da2dd5f045447fa8bdc31e6b653
pawel <pawel.wyzykowski@coderslab.pl>
2018-02-05 22:28:28 +0100

    changed models a little

--- bitcraft/protected_links/models.py
+++ bitcraft/protected_links/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Links(models.Model):
     address = models.URLField(max_length=1000)
     password = models.CharField(max_length=20)
-    link = models.URLField(max_length=1000)
+    token = models.CharField(max_length=1000)
     entries = models.IntegerField()
     add_date = models.DateField(auto_now_add=True)
 
@@ -13,6 +13,6 @@ class Files(models.Model):
     id = models.AutoField(primary_key=True)
     file = models.FileField()
     password = models.CharField(max_length=20)
-    link = models.URLField(max_length=1000)
+    token = models.CharField(max_length=1000)
     entries = models.IntegerField()
     add_date = models.DateField(auto_now_add=True)
